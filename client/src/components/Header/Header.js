aa47002173d65e9b9fd01c006e721bd852361a8c
Vasyl Frantsishkevych <vasul.fr@gmail.com>
2022-11-24 23:39:16 +0200

    front part

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import {Container, Nav, Navbar} from "react-bootstrap";
+import React, {useContext} from 'react';
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import {SHOP_ROUTE} from "../../utils";
+import {Context} from "../../index";
+import {observer} from "mobx-react-lite";
 
-const Header = () => {
+const Header = observer(() => {
+    const {user} = useContext(Context);
+
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="dark" variant="dark">
             <Container>
-                <NavLink style={{color: 'gray'}} to={SHOP_ROUTE}>SHOP Device</NavLink>
-                <Nav className="ml-auto">
-
-                </Nav>
+                <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>SHOP Device</NavLink>
+                {user.isAuth ?
+                    <Nav className="ms-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-light"}>Адміністратор</Button>
+                        <Button variant={"outline-light"} className="ms-2">Ввійти</Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Зареєструватися</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
     );
-};
+});
 
 export {Header};
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils";
 
 const AuthPage = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
-        <div>
-            Auth
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизація' : 'Реєстрація'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введіть email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введіть пароль..."
+                    />
+                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <div>
+                                Незареєстрований? <NavLink to={REGISTRATION_ROUTE}>Зареєструватися!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Зареєстрований? <NavLink to={LOGIN_ROUTE}>Ввійти!</NavLink>
+                            </div>
+                        }
+                        <Button variant={"outline-success"}>
+                            {
+                                isLogin ? 'Вхід' : 'Зареєструватися'
+                            }
+                        </Button>
+                    </Row>
+                </Form>
+
+            </Card>
+        </Container>
     );
 };
 
