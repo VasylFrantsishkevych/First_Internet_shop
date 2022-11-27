c8a3f77fc5574c40bcff445643f4b3ec39853afe
VF <vasul.fr@gmail.com>
2022-11-27 13:50:59 +0200

    sds

---
+++ client/src/components/TypeBar/TypeBar.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../../index";
+
+const TypeBar = observer(() => {
+    const {device} = useContext(Context);
+
+    return (
+        <ul className="list-group">
+            {device.types.map(type =>
+                <li
+                    style={{cursor: 'pointer'}}
+                    active={type.id === device.selectedType.id ? 'danger' : 'light'}
+                    onClick={() => device.setSelectedType(type)}
+                    className="list-group-item"
+                    key={type.id}>
+                    {type.name}
+                </li>
+            )}
+        </ul>
+    );
+});
+
+export {TypeBar};
--- client/src/pages/ShopPage.js
+++ client/src/pages/ShopPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import {BrandBar, TypeBar} from "../components";
 
 const ShopPage = () => {
     return (
-        <div>
-            Shop
-        </div>
+        <Container className="mt-2">
+            <Row>
+                <Col md={3}>
+                    <TypeBar/>
+                </Col>
+                <Col md={9}>
+                    <BrandBar/>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
